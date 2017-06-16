68852ec7416c8d7be55f0600966c7e9c460b4991
iareuniqe <1062698930@qq.com>
2017-06-16 16:27:21 +0800

    一 对多
    增加了一对多的示例

--- mybatis/src/main/java/com/lei/test/Test.java
+++ mybatis/src/main/java/com/lei/test/Test.java
@@ -1,9 +1,14 @@
 package com.lei.test;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.bean.model.People;
+import com.bean.model.Post;
 import com.lei.bean.User;
+import com.lei.dao.PeopleDao;
 import com.lei.dao.UserDao;
 
 public class Test {
@@ -14,14 +19,19 @@ public class Test {
         String[] locations = {"beans.xml"};     
         ApplicationContext ctx =      
             new ClassPathXmlApplicationContext(locations);  
-        UserDao userDao=(UserDao) ctx.getBean("userdaoimpl");
-        User user=userDao.getUser(2);        
-        System.out.println(user);
+      //  UserDao userDao=(UserDao) ctx.getBean("userdaoimpl");
+        //User user=userDao.getUser(2);        
+        //System.out.println(user);
        // userDao.addUser(new User(4, "yang"));
         //userDao.deleteUser(4);
         //userDao.deleteUser("lei");
         //userDao.updateUser(new User(2, "wang"));
-        
+        PeopleDao peopleDao=(PeopleDao) ctx.getBean("peopledaoimple");
+        People people=peopleDao.getPost(1);
+        List<Post> list=people.getPosts();
+        for (Post post : list) {
+			System.out.println(post);
+		}
         
 	}
 
